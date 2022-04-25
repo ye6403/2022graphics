c3f682d77c14b78a4876e55f55abaf1b99c348df
ye6403 <101086287+ye6403@users.noreply.github.com>
2022-04-25 16:47:45 +0800

    Add files via upload

---
+++ week10_texture_backgroung/main.cpp
@@ -0,0 +1,48 @@
+#include <opencv/highgui.h> ///使用 OpenCV 2.1 比較簡單, 只要用 High GUI 即可
+#include <opencv/cv.h>
+#include <GL/glut.h>
+#include "glm.h"
+GLUquadric * sphere = NULL;
+int myTexture(char * filename)
+{
+    IplImage * img = cvLoadImage(filename); ///OpenCV讀圖
+    cvCvtColor(img,img, CV_BGR2RGB); ///OpenCV轉色彩 (需要cv.h)
+    glEnable(GL_TEXTURE_2D); ///1. 開啟貼圖功能
+    GLuint id; ///準備一個 unsigned int 整數, 叫 貼圖ID
+    glGenTextures(1, &id); /// 產生Generate 貼圖ID
+    glBindTexture(GL_TEXTURE_2D, id); ///綁定bind 貼圖ID
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); /// 貼圖參數, 超過包裝的範圖T, 就重覆貼圖
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); /// 貼圖參數, 超過包裝的範圖S, 就重覆貼圖
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); /// 貼圖參數, 放大時的內插, 用最近點
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); /// 貼圖參數, 縮小時的內插, 用最近點
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img->width, img->height, 0, GL_RGB, GL_UNSIGNED_BYTE, img->imageData);
+	return id;
+}
+float angle=0;
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glPushMatrix();
+        glRotatef(angle,0,1,0);
+        if(pmodel == NULL){
+            pomdel= glmReadOBJ("data/Gundam.obj");
+            glmUnitize(pmodel);
+            glmFaceNormals(pmodel);
+            glmVertexNormals(pmodel,90);
+    }
+    glmDraw(pmodel,GLM_MATERIAL | GLM_TEXTURE);
+    glPopMatrix();
+    glutSwapBuffers();
+    angle++;
+}
+int main(int argc,char**argv)
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("week10 texture background");
+    glutDisplayFunc(display);
+    myTexture("data/Diffuse.jpg");
+    sphere = gluNewQuadric();
+    glEnable(GL_DEPTH_TEST);
+    glutMainLoop();
+}
